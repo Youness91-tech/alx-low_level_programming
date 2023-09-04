ec5735c96ad381a80c950c9d94ee25a98b7555b9
Youness91-Tech <bayttarbawi.com@gmail.com>
2023-09-04 15:44:42 -0700

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - copies the content of a file to another file.
+ * @argc: The argument counter.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	int op_from, op_dest, rd, w;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = create_buffer(argv[2]);
+	op_from = open(argv[1], O_RDONLY);
+	rd = read(op_from, buffer, 1024);
+	op_dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (op_from == -1 || rd == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		w = write(op_dest, buffer, rd);
+		if (op_dest == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		rd = read(op_from, buffer, 1024);
+		op_dest = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (rd > 0);
+	free(buffer);
+	close_file(op_from);
+	close_file(op_dest);
+	return (0);
+}
